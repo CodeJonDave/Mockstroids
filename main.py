58b1fb34d4c2d39a9426d1b7926a9ba045899860
CodeJonDave <jonathan.d.sutton2024@gmail.com>
2025-09-25 21:12:21 -0700

    #4 added pygame clock and dt tracker

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Starting Mockstroids — {SCREEN_WIDTH}x{SCREEN_HEIGHT}")  # noqa: F405
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # noqa: F405
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+    dt = clock.tick(60) / 1000  # noqa: F841
 
 
 if __name__ == "__main__":
