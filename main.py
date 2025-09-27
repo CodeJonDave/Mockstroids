c7a8bc93c92ad7d45e52ca2d54770f427568b52d
CodeJonDave <jonathan.d.sutton2024@gmail.com>
2025-09-27 01:43:28 -0700

    #6 Added the player class file and added to the main file, and drew to screen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
+from player import Player
 from constants import *  # noqa: F403
 
 
 def main():
     print(f"Starting Mockstroids — {SCREEN_WIDTH}x{SCREEN_HEIGHT}")  # noqa: F405
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # noqa: F405
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # noqa: F405
     clock = pygame.time.Clock()
     dt = 0
 
@@ -15,6 +17,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # noqa: F841
