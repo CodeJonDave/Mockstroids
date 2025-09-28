6ce56029a6ed45b4357454fd0c0d82d6af66c719
CodeJonDave <jonathan.d.sutton2024@gmail.com>
2025-09-27 19:25:11 -0700

    #7 added in the movement for the player both clockwise/counterclockwise rotation and forward/backward movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_MOVE_SPEED
 from circleshape import CircleShape
 
 
@@ -10,7 +10,7 @@ class Player(CircleShape):
         self.rotation = 0
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
@@ -19,3 +19,22 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_MOVE_SPEED * dt
+
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        elif keys[pygame.K_d]:
+            self.rotate(dt)
+        elif keys[pygame.K_w]:
+            self.move(-dt)
+        elif keys[pygame.K_s]:
+            self.move(dt)
