843e8b508a4eda979b13f4091a2fb2ac38dd7a9c
CodeJonDave <jonathan.d.sutton2024@gmail.com>
2025-09-28 21:57:19 -0700

    #15 added score counter

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ from constants import * # noqa: F403
 
 
 def main():
+    player_score = 0
+    
     print(f"Starting Mockstroids — {SCREEN_WIDTH}x{SCREEN_HEIGHT}")  # noqa: F405
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # noqa: F405
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    pygame.font.init()
+    font = pygame.font.Font(None, 28)# noqa: F405
     
     asteroids = pygame.sprite.Group()
     updatables = pygame.sprite.Group()
@@ -39,6 +44,7 @@ def main():
             for asteroid in asteroids:
                 if shot.isColliding(asteroid):
                     shot.kill()
+                    player_score += 1
                     asteroid.split()
                     break
             
@@ -50,6 +56,10 @@ def main():
         for drawable in drawables:
             drawable.draw(screen)
             
+        score_surf = font.render(F"Score: {player_score}", True, "white")
+        score_rect = score_surf.get_rect(topright=(SCREEN_WIDTH - 10, 10))  # noqa: F405
+        screen.blit(score_surf,score_rect)
+        # noqa: F405
         pygame.display.flip()
 
 
